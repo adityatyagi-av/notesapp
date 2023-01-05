899d2e9dd6207e9f5dcf3d7d733ef1bc1edd7468
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-05 22:41:31 +0530

    added files for notes app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Notes from './components/Notes'
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
+  }
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+   <Notes notes={notes}/>
+  </>
   );
 }
 
---
+++ src/components/Notes.js
@@ -0,0 +1,60 @@
+// import { eventWrapper } from '@testing-library/user-event/dist/utils'
+import React,{useState} from 'react'
+
+const Note =({note})=>{
+    return(
+        <li>{note.content}</li>
+    )
+}
+
+export default function Notes(props) {
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState(
+        'a new note'
+    )
+    const [showAll, setShowAll] = useState(true)
+
+    const notesToShow = showAll
+    ? notes
+    :notes.filter(note=>note.important === true)
+
+
+
+    const addNote =(event)=>{
+        event.preventDefault()
+        console.log('button clicked',event.target)
+        const noteObject ={
+            content: newNote,
+            date: new Date().toISOString,
+            important: Math.random() < 0.5,
+            id: notes.length +1
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote('')
+      
+    }
+
+    const handleNoteChange=(event)=>{
+        console.log(event.target.value)
+        setNewNote(event.target.value)
+    }
+  return (
+    <div>
+
+        <h1>Notes</h1>
+        <button onClick={()=> setShowAll(!showAll)}>show {showAll?'important':'all'}</button>
+        <ul>
+            {notesToShow.map(note =>
+            { return(
+                <Note key={note.id} note={note}/>
+            )})
+            }
+        </ul>
+        <form onSubmit={addNote}>
+            
+            <input value={newNote} onChange={handleNoteChange}/>
+            <button type='submit'>Save</button>
+        </form>
+    </div>
+  )
+}
