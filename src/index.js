34b54f6bced336b692385a08b869f72a753bd97b
Aditya Tyagi <adityatyagiav1@gmail.com>
2023-01-06 22:45:29 +0530

    server setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import axios from 'axios'
+import Notes from './components/Notes';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// axios.get('http://localhost:3001/notes')
+// const promise2=axios.get('http://localhost:3001/foobar')
+axios
+  .get('http://localhost:3001/notes')
+  .then(response=>{
+   const notes = response.data
+   console.log(notes)
+   const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Notes notes={notes}/>
   </React.StrictMode>
 );
+  })
+
+// console.log(promise2)
+
